067859165c3ab0363dcb83fa593abb8d64fee140
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2023-10-03 21:02:30 +0530

    Time: 394 ms (5.13%) | Memory: 129.5 MB (7.37%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
+    vector<int> countSort(vector<int>& nums){
+      
+        map<int,int> mp;
+        for(auto it : nums) mp[it]++;
+        nums.clear();
+        for(auto it : mp){
+            int count = it.second;
+            while(count--){
+                nums.push_back(it.first);
+            }
+        }
+        return nums;
+    }
     int maximumGap(vector<int>& nums) {
+        if(nums.size() == 1) return 0;
+        vector<int> temp = countSort(nums);
+        int maxGap = INT_MIN;
+        for(int i = 1; i < temp.size(); i++){
+            int gap = temp[i] -temp[i-1];
+            maxGap = max(maxGap,gap);
+        }
+        return maxGap;
+        /*
         if(nums.size() == 1) return 0;
         sort(nums.begin(),nums.end());
         int maxGap = INT_MIN;
@@ -9,6 +31,6 @@ public:
             int gap = nums[i] - nums[i-1];
             maxGap = max(maxGap,gap);
         }
-        return maxGap; 
+        return maxGap; */
     }
 };
