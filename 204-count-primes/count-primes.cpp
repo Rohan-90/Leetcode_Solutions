2e67ebbdae0d56533635886b88ff7e1ff461811e
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2023-10-04 12:36:12 +0530

    Time: 668 ms (16.99%) | Memory: 10.6 MB (29.06%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+   /* bool isPrime(int a){
+       
+        if(a <= 1) return false;
+        for(int i = 2; i <= sqrt(a); i++){
+            if(a % i == 0) return false;
+        }
+        return true;
+    }*/
+    int countPrimes(int n) {
+        if(n == 0) return 0;
+        int count = 0;
+        vector<bool> vec(n,true);
+        vec[0] == vec[1] == false;
+        for(int i = 2; i < n; i++){
+            if(vec[i]){
+                count++;
+                int j = 2 * i;
+                while(j < n){
+                    vec[j] = false;
+                    j += i;
+                }
+            }
+        }
+        return count;
+
+     /*   int count = 0;
+        for(int i = 0; i < n; i++){
+            if(isPrime(i)) count++;
+        }
+        return count;*/
+    }
+};
