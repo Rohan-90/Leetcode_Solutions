fc5ebb3b21e06c2a7b0f8f51d98e42df8fe7d311
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2023-11-07 22:17:28 +0530

    Time: 3 ms (31.16%) | Memory: 6.8 MB (53.87%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dpMemo(int i, int j, int m, int n, vector<vector<int>>& dp){
         if(i >= m || j >= n) return 0;
-        if(i >= m - 1 && j >= n - 1) return 1;
+        if(i == m - 1 && j == n - 1) return 1;
         if(dp[i][j] != -1) return dp[i][j];
         return dp[i][j] = dpMemo(i + 1, j , m, n, dp) + dpMemo(i, j + 1, m, n, dp);
     }
