a048efe3d37414a95617e74e2ebe931f6c395864
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2023-11-27 22:41:08 +0530

    Time: 4 ms (30.51%) | Memory: 6.2 MB (94.14%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int mod = 1e9 + 7;
+        int count = 0;
+        while(n){
+            count += n % 2; 
+            n = n >> 1;
+        }
+        return count;
+    }
+};
