515b94688740ef57a5f57666f9fb05814f515352
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2023-12-01 00:37:04 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (74.22%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -8,11 +8,6 @@ public:
             ans = (ans * 10) + mod;
             x = x / 10;
         }
-        // ans * 10 + mod > INT_MAX then 
-        // ans > (INT_MAX - mod) / 10; as mod range is 0-9 
-        //(0-9)/ 10 is 0 in int  so the total equation we can write is ans > INT_MAX/10 
-        // ans < INT_MIN/10
-        // same case in case of INT_MIN 
         return ans;
     }
 };
