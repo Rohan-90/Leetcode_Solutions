dcbef8ab68038a018731237eaf56955068c65707
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2023-12-11 15:17:08 +0530

    Time: 13 ms (42.19%) | Memory: 12.6 MB (92.26%) - LeetSync

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        if(arr.size() == 1) return arr[0];
+        int maxCount = 0;
+        int element = -1;
+        int count = 1;
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] == arr[i - 1]) count++;
+            else count  = 1;
+            if(count >= maxCount){
+                maxCount = count;
+                element = arr[i]; 
+            }
+            
+        }
+        return element;
+    }
+};
