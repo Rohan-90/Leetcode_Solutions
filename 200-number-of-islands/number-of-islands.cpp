2553cf56fb24c61813bdd2e57072bd578db32910
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2024-03-05 01:08:30 +0530

    Time: 29 ms (57.13%) | Memory: 15.7 MB (70.59%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& grid, int row, int col){
+        if(row >= grid.size() || col >= grid[0].size() || row < 0 || col < 0 || grid[row][col] == '0') return;
+        grid[row][col] = '0';
+        solve(grid, row + 1, col);
+        solve(grid, row - 1, col);
+        solve(grid, row, col + 1);
+        solve(grid, row, col - 1);
+
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for(int  i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == '1'){
+                    solve(grid, i ,j);
+                    count++;
+                }
+               
+            }
+        }
+        return count;
+    }
+};
