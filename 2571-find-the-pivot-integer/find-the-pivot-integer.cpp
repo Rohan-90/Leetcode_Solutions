3c429a9cbbaa8a05f39145334e71ce69064928b5
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2024-03-13 12:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (12.00%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        if(n == 1) return 1;
-        unordered_map<int,int> m;
-        int sum = 0;
-        for(int i = 1; i < n; i++){
-            sum += i;
-            m[sum] = i;
-        }
-        int j = n;
-        int sSum = 0;
-        while(j > 0){
-            sSum += j;
-            if(m.find(sSum) != m.end() && (m[sSum] == j)) return m[sSum];
-            j--;
-        }
-        return -1;
+        double x = sqrt(n * (n + 1) / 2.0);
+        if (fmod(x, 1.0) != 0) return -1;
+        
+        return static_cast<int>(x);
+        
+        // if(n == 1) return 1;
+        // unordered_map<int,int> m;
+        // int sum = 0;
+        // for(int i = 1; i < n; i++){
+        //     sum += i;
+        //     m[sum] = i;
+        // }
+        // int j = n;
+        // int sSum = 0;
+        // while(j > 0){
+        //     sSum += j;
+        //     if(m.find(sSum) != m.end() && (m[sSum] == j)) return m[sSum];
+        //     j--;
+        // }
+        // return -1;
     }
 };
