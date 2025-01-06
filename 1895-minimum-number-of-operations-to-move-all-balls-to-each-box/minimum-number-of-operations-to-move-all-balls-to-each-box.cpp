8c1affc94437d8446089093c53884aafb479d820
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2025-01-06 18:41:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (67.01%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> ans(boxes.size());
-        vector<pair<int,int>> ones;
+        vector<int> ans(boxes.size(),0);
+        // vector<int> ones;
+        // for(int i = 0; i < boxes.size(); i++) {
+        //     if(boxes[i] == '1') ones.push_back(i);
+        // }
+        // for(int i = 0; i < boxes.size(); i++) {
+        //     int num = 0;
+        //     for(auto it : ones) {
+        //         num += abs(it - i);
+        //     }
+        //     ans[i] = num;
+        // }
+        // return ans;
+        int value = 0, cumSum = 0;
         for(int i = 0; i < boxes.size(); i++) {
-            if(boxes[i] == '1') ones.push_back({1,i});
+            ans[i] = cumSum;
+            if(boxes[i] == '0') value += 0;
+            else value += 1;
+            cumSum += value;
         }
-        for(int i = 0; i < boxes.size(); i++) {
-            int num = 0;
-            for(auto it : ones) {
-                num += abs(it.second - i);
-            }
-            ans[i] = num;
+        value = 0;
+        cumSum = 0;
+        for(int i = boxes.size() - 1; i >= 0; i--) {
+            ans[i] += cumSum;
+            if(boxes[i] == '0') value += 0;
+            else value += 1;
+            cumSum += value;
         }
         return ans;
     }
