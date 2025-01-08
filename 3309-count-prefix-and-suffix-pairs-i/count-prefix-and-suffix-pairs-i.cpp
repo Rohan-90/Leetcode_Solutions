64103947c8e560671fdefe18c0de5c11c223cc28
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2025-01-08 20:02:52 +0530

    Time: 4 ms (52.97%) | Memory: 23.3 MB (90.68%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int ans = 0;
+        for(int i = 0; i < words.size(); i++) {
+            for(int j = i + 1; j < words.size(); j++) {
+                if(words[j].find(words[i]) == 0 && words[j].rfind(words[i]) == words[j].size() - words[i].size()) ans++;
+            }
+        }
+        return ans;
+    }
+};
