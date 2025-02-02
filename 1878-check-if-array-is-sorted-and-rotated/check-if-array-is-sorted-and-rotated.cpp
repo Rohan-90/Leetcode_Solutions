dabc632a9a2c72075af33217fd0cceca8d1b38c5
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2025-02-02 22:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (58.89%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    bool solve(vector<int>& ans){
-        for(int i = 0; i < ans.size() - 1; i++){
-            if(ans[i] > ans[i + 1]) return false;
-        }
-        return true;
-    }
+    // bool solve(vector<int>& ans){
+    //     for(int i = 0; i < ans.size() - 1; i++){
+    //         if(ans[i] > ans[i + 1]) return false;
+    //     }
+    //     return true;
+    // }
     bool check(vector<int>& nums) {
-        vector<int> ans(nums.size());
-        int k = 0;
-        while(k < nums.size()){
-            for(int i = 0 ; i < nums.size(); i++){
-            ans[i] = nums[(i + k)% nums.size()];
+        // vector<int> ans(nums.size());
+        // int k = 0;
+        // while(k < nums.size()){
+        //     for(int i = 0 ; i < nums.size(); i++){
+        //     ans[i] = nums[(i + k)% nums.size()];
+        //     }
+        //     if(solve(ans) == true) return true;
+        //     k++;
+        // }
+        // return false;
+        int count = 0;
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(i == 0 && nums[nums.size() - 1] > nums[i]) count++;
+            if(nums[i] > nums[i + 1]){
+                count++;
             }
-            if(solve(ans) == true) return true;
-            k++;
         }
-        return false;
+        return count <=1;
     }
 };
