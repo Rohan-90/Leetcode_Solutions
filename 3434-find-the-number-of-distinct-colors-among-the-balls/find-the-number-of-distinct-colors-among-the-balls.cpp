ba99d7ed696b056b6911fae62bbdfcc3b9b87cc0
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2025-02-08 14:04:40 +0530

    Time: 112 ms (64.89%) | Memory: 157.5 MB (60.81%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        unordered_set<int> distinct;
         vector<int> ans;
-        unordered_map<int, int> mp;
-        unordered_map<int, int> freq;
-
-        for (int i = 0; i < queries.size(); i++) {
-            int key = queries[i][0];
-            int value = queries[i][1];
-
-            if (mp.find(key) != mp.end()) {
-                int oldValue = mp[key];
-                freq[oldValue]--;
-                if (freq[oldValue] == 0) distinct.erase(oldValue);
+        unordered_map<int,int> counts;
+        unordered_map<int,int> prevColor;
+        for(int i = 0; i < queries.size(); i++) {
+            int ball = queries[i][0];
+            int color = queries[i][1];
+            if(prevColor.count(ball)) {
+                int prev = prevColor[ball];
+                counts[prev]--;
+                if(counts[prev] == 0) counts.erase(prev);
             }
-
-            mp[key] = value;
-            freq[value]++;
-            distinct.insert(value);
-            ans.push_back(distinct.size());
+            prevColor[ball] = color;
+            counts[color]++;
+            ans.push_back(counts.size());
         }
+        
         return ans;
     }
 };
